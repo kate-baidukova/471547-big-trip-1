eb76ccd48b73efb45fc2a26e121d7cb8f7230eaf
Ekaterina Baidukova <kate.baidukova@gmail.com>
2024-02-17 18:04:43 +0300

    Добавляет вывод форм добавления и ред. карточек

--- presenter/trip-presenter.js
+++ presenter/trip-presenter.js
@@ -1,6 +1,8 @@
-import {render} from '../src/render.js';
+import {render, RenderPosition} from '../src/render.js';
 import TripEventsList from '../view/trip-list-view.js';
 import TripEvent from '../view/trip-event-view.js';
+import FormPointAdd from '../view/point-form-new-view.js';
+import FormPointEdit from '../view/point-edit-form-view.js';
 
 const POINT_COUNT = 3;
 
@@ -12,10 +14,12 @@ export default class TripPresenter {
   }
 
   init () {
+    render(new FormPointAdd, this.mainTripEventsContainer, RenderPosition.BEFOREEND);
+    render(new FormPointEdit, this.mainTripEventsContainer, RenderPosition.BEFOREEND);
     render(this.tripListComponent, this.mainTripEventsContainer);
 
     for (let i = 0; i < POINT_COUNT; i++) {
-      render(new TripEvent(), this.tripListComponent.getElement());
+      render(new TripEvent, this.tripListComponent.getElement());
     }
   }
 }
